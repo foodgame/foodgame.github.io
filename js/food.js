5a27dc564dbdf25bcd81e874373a16f02879b34b
foodgame <33816735+foodgame@users.noreply.github.com>
2020-02-01 16:01:28 +0800

    commit

--- js/food.js
+++ js/food.js
@@ -153,6 +153,10 @@ function updateTooltip() {
                 trigger: "hover"
             }
         );
+
+        $('[data-toggle="tooltip"]').on('show.bs.tooltip', function () {
+            $('[data-toggle="tooltip"]').not(this).tooltip('hide');
+        });
     } else {
         $('[data-toggle="tooltip"]:not(.tooltip-pin)').tooltip('destroy');
     }
